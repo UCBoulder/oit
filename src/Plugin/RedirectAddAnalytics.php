223605e9f0e5c5932a7e4a4f5c18703fda1c2244
protitude <miles.france@gmail.com>
2023-03-06 22:22:19 +0000

    RedirectAddAnalytics: adding plugin

---
+++ src/Plugin/RedirectAddAnalytics.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\oit\Plugin;
+
+/**
+ * Environment icon to be used on header title.
+ *
+ * @RedirectAddAnalytics(
+ *   id = "redirect_add_analytics",
+ *   title = @Translation("Redirect add analytics"),
+ *   description = @Translation("Query for redirects and add the proper
+ *   utm info. Will run nightly in cron.")
+ * )
+ */
+class RedirectAddAnalytics {
+  /**
+   * Look for redirects missing utm code and add it.
+   */
+  public function __construct() {
+    $query = \Drupal::database()->select('redirect', 'r');
+    $query->fields('r', ['rid', 'redirect_source__path', 'redirect_redirect__uri']);
+    $query->condition('redirect_redirect__uri', '%?utm%', 'NOT LIKE');
+    $query->range(0, 500);
+    $redirects = $query->execute()->fetchAll();
+    foreach ($redirects as $redirect) {
+      $uri = $redirect->redirect_redirect__uri;
+      $path = $redirect->redirect_source__path;
+      $query = \Drupal::database()->update('redirect');
+      $query->condition('rid', $redirect->rid);
+      $query->fields([
+        'redirect_redirect__uri' => $uri . '?utm_source=' . $path . '&utm_campaign=redirect',
+      ]);
+      $query->execute();
+    }
+  }
+}
