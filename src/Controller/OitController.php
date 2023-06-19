2fff1a7ec6ce079cb0711ced1338eee052f171b7
protitude <miles.france@gmail.com>
2023-06-19 18:23:10 +0000

    OitController/AbsoluteLinkShame: clean up codes

--- src/Controller/OitController.php
+++ src/Controller/OitController.php
@@ -5,7 +5,6 @@ namespace Drupal\oit\Controller;
 use Drupal\Component\Utility\Xss;
 use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Datetime\DateFormatter;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\PageCache\ResponsePolicy\KillSwitch;
@@ -16,6 +15,10 @@ use Drupal\views\Views;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Drupal\Core\Extension\ModuleExtensionList;
+use Drupal\oit\Plugin\BlockUuidQuery;
+use Drupal\oit\Plugin\ServiceHealth;
+use Drupal\Core\Render\RendererInterface;
 
 /**
  * Controller routines for zap routes.
@@ -28,6 +31,13 @@ class OitController extends ControllerBase {
    * @var \Drupal\Core\Session\AccountInterface
    */
   protected $account;
+
+  /**
+   * Object used to get request data, such as the hash.
+   *
+   * @var \Drupal\oit\Plugin\BlockUuidQuery
+   */
+  protected $blockUuidQuery;
 
   /**
    * Object used to get request data, such as the hash.
@@ -58,11 +68,25 @@ class OitController extends ControllerBase {
   protected $configFactory;
 
   /**
+   * The 'renderer' service.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
+   */
+  protected $renderer;
+
+  /**
    * Access date formatter.
    *
-   * @var \Drupal\Core\Datetime\DateFormatter
+   * @var \Drupal\oit\Plugin\ServiceHealth
    */
-  private $dateFormatter;
+  private $serviceHealth;
+
+  /**
+   * The ModuleExtensionList to be passed to the config importer.
+   *
+   * @var \Drupal\Core\Extension\ModuleExtensionList
+   */
+  protected $moduleExtensionList;
 
   /**
    * The entity type manager.
@@ -76,6 +100,8 @@ class OitController extends ControllerBase {
    *
    * @param \Drupal\Core\Session\AccountInterface $account
    *   Interact with Private temporary storage.
+   * @param \Drupal\oit\Plugin\BlockUuidQuery $block_uuid_query
+   *   Get block uuid.
    * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
    *   Access to the current request, including to session objects.
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
@@ -84,8 +110,12 @@ class OitController extends ControllerBase {
    *   The page cache kill switch service.
    * @param \Drupal\Core\Config\ConfigFactory $config_factory
    *   Interact with config factory.
-   * @param \Drupal\Core\Datetime\DateFormatter $date_formatter
-   *   Interact with date formatter.
+   * @param \Drupal\Core\Extension\ModuleExtensionList $module_extension_list
+   *   Load module extension list.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The 'renderer' service.
+   * @param \Drupal\oit\Plugin\ServiceHealth $service_health
+   *   Load service health.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   Load entity.
    */
@@ -95,7 +125,10 @@ class OitController extends ControllerBase {
     LoggerChannelFactoryInterface $logger_factory,
     KillSwitch $killSwitch,
     ConfigFactory $config_factory,
-    DateFormatter $date_formatter,
+    BlockUuidQuery $block_uuid_query,
+    ModuleExtensionList $module_extension_list,
+    RendererInterface $renderer,
+    ServiceHealth $service_health,
     EntityTypeManagerInterface $entity_type_manager
   ) {
     $this->account = $account;
@@ -103,22 +136,28 @@ class OitController extends ControllerBase {
     $this->loggerFactory = $logger_factory->get('oit');
     $this->killSwitch = $killSwitch;
     $this->configFactory = $config_factory;
-    $this->dateFormatter = $date_formatter;
+    $this->blockUuidQuery = $block_uuid_query;
+    $this->moduleExtensionList = $module_extension_list;
+    $this->renderer = $renderer;
+    $this->serviceHealth = $service_health;
     $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container): self {
     $teamsAlert = $container->get('oit.teamsalert');
-    return new static(
+    return new self(
       $container->get('current_user'),
       $container->get('request_stack'),
       $container->get('logger.factory'),
       $container->get('page_cache_kill_switch'),
       $container->get('config.factory'),
-      $container->get('date.formatter'),
+      $container->get('oit.block.uuid.query'),
+      $container->get('extension.list.module'),
+      $container->get('renderer'),
+      $container->get('oit.servicehealth'),
       $container->get('entity_type.manager'),
       $teamsAlert
     );
@@ -152,7 +191,7 @@ class OitController extends ControllerBase {
       $requested_path = '';
     }
     global $base_url;
-    $module_path = \Drupal::service('extension.list.module')->getPath('oit');
+    $module_path = $this->moduleExtensionList->getPath('oit');
     $content = sprintf(
       '<p>%s <a href="%s/saml_login%s">%s</a> %s.</p><a style="border: none;" href="%s/saml_login%s"><img src="%s/%s/images/you_shall_not_pass.png" alt="%s" title="%s" style="display:none;" id="myprecious" /></a>',
       $this->t('You may need to'),
@@ -203,17 +242,17 @@ class OitController extends ControllerBase {
     $svg = new ShortcodeIcon();
     $title = $this->t('Request Portal');
     $search = '<form action="/search/cse" method="get" id="search-request" accept-charset="UTF-8" class="search" data-drupal-form-fields="edit-keys"><label for="edit-keys" class="visually-hidden form-item__label">Search</label> <input title="Enter the terms you wish to search for." placeholder="Get help with..." autocomplete="off" type="search" id="search-keys" name="keys" value="" size="15" maxlength="128" class="form-search form-item__textfield"><input data-drupal-selector="submit-search" type="submit" id="submit-search" value="Search" class="button"></form>';
-    $issue_query = \Drupal::service('oit.block.uuid.query');
+    $issue_query = $this->blockUuidQuery;
     $issue_query->getBidByUuid('04d5cc3e-8b9d-4bb7-8cab-16f162cb729a');
     $issue = $issue_query->loadBlock();
-    $request_query = \Drupal::service('oit.block.uuid.query');
+    $request_query = $this->blockUuidQuery;
     $request_query->getBidByUuid('3a69be06-3b81-4a30-9996-0da3d6c45e8d');
     $request = $request_query->loadBlock();
-    $cases_query = \Drupal::service('oit.block.uuid.query');
+    $cases_query = $this->blockUuidQuery;
     $cases_query->getBidByUuid('f4bc74b5-ba88-4b86-86e4-58d9ce96260b');
     $my_cases = $cases_query->loadBlock();
 
-    $topreq_query = \Drupal::service('oit.block.uuid.query');
+    $topreq_query = $this->blockUuidQuery;
     $topreq_query->getBidByUuid('9c8e8f57-f654-4976-a4bd-85acb21f0457');
     $topreq = $topreq_query->loadBlock();
     $view_id = 'request_portal';
@@ -268,7 +307,7 @@ class OitController extends ControllerBase {
     ];
 
     return [
-      '#markup' => render($page),
+      '#markup' => $this->renderer->render($page),
       '#allowed_tags' => ['a', 'form', 'label', 'input', 'svg', 'use'],
     ];
   }
@@ -309,7 +348,7 @@ class OitController extends ControllerBase {
    * Service Alert dashboard page.
    */
   public function serviceAlertHealth() {
-    $service_dashboard = \Drupal::service('oit.servicehealth');
+    $service_dashboard = $this->serviceHealth;
     $category = $service_dashboard->serviceHealthLookup();
     $status_key = $service_dashboard->serviceHealthStatusByKey();
     $clean_category = $service_dashboard->removeDuplicates($category);
@@ -343,7 +382,7 @@ class OitController extends ControllerBase {
     ];
 
     return [
-      '#markup' => render($build),
+      '#markup' => $this->renderer->render($build),
       '#allowed_tags' => ['svg', 'circle'],
     ];
   }
