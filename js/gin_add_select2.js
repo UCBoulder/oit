1d5d8e19ca770a9c6be77e1c6b3628f0f88d2728
protitude <miles.france@gmail.com>
2023-07-19 21:22:22 +0000

    gin: adding title/css/js for theme

---
+++ js/gin_add_select2.js
@@ -0,0 +1,6 @@
+(function ($, Drupal) {
+  $( document ).ready(function () {
+    $("#edit-field-tut-ref-service, #edit-field-tut-ref-tut, #edit-field-oit-category, #edit-field-tut-back-btn").select2({
+    });
+  });
+})(jQuery, Drupal);
