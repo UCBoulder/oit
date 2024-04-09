5099a2e5c29c6821f211eab0c919dc5fe3fdc165
protitude <miles.france@gmail.com>
2024-04-08 18:16:28 -0600

    drush: fix phpstan error

--- src/Commands/OitCommands.php
+++ src/Commands/OitCommands.php
@@ -3,6 +3,8 @@
 namespace Drupal\oit\Commands;
 
 use Drush\Commands\DrushCommands;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\servicenow\Plugin\PrincessList;
 
 /**
  * Various utility commands for OIT.
@@ -10,15 +12,53 @@ use Drush\Commands\DrushCommands;
 class OitCommands extends DrushCommands {
 
   /**
-   * FULL Rebuild Princess List.
+   * Princess List.
+   *
+   * @var \Drupal\servicenow\Plugin\PrincessList
+   */
+  protected $princessList;
+
+  /**
+   * The Messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Construct object.
+   */
+  public function __construct(
+    PrincessList $princess_list,
+    MessengerInterface $messenger
+  ) {
+    parent::__construct();
+    $this->princessList = $princess_list;
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * Rebuild Princess List.
    *
    * @command oit:reload-princess
-   * @aliases oit-rp
+   * @aliases oit:rp
    */
   public function reloadPrincess() {
-    $princess = \Drupal::service('servicenow.princess.list');
-    $princess->reload();
-    $princess->cron(0);
+    $this->princessList->reload();
+    $this->messenger->addMessage('Princess List reloaded.');
+    $this->loadPrincess();
+  }
+
+
+  /**
+   * Load Princess List.
+   *
+   * @command oit:load-princess
+   * @aliases oit:lp
+   */
+  public function loadPrincess() {
+    $this->princessList->cron(0);
+    $this->messenger->addMessage('Princess List Loaded.');
   }
 
 }
