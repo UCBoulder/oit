588501fd20570faf4ec7f4b00bff3f997ac92cc3
protitude <miles.france@gmail.com>
2024-08-14 11:26:32 -0600

    services: adding perms to user edit page

---
+++ src/Services/RouteSubscriber.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\oit\Services;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('entity.user.edit_form')) {
+      $route->setRequirement('_permission', 'administer users');
+    }
+  }
+
+}
