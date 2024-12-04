00eb499eca4958cb6ec008d1a8c43fa7092d7805
protitude <miles.france@gmail.com>
2024-12-04 14:13:11 -0700

    phpcs fix

--- src/Plugin/Block/ServiceAlertTweet.php
+++ src/Plugin/Block/ServiceAlertTweet.php
@@ -111,7 +111,7 @@ class ServiceAlertTweet extends BlockBase implements
     $route_match = $this->routeMatchInterface->getRawParameters()->getIterator();
     $node_title = $this->entityTypeManager->getStorage('node')->load($route_match['node'])->getTitle();
     $node_type = $this->entityTypeManager->getStorage('node')->load($route_match['node'])->bundle();
-    $service_status = $node_type == 'service_alert' ? $this->entityTypeManager->getStorage('node')->load($route_match['node'])->get('field_service_alert_status')->value . ': '  : '';
+    $service_status = $node_type == 'service_alert' ? $this->entityTypeManager->getStorage('node')->load($route_match['node'])->get('field_service_alert_status')->value . ': ' : '';
     $host = $this->request->getCurrentRequest()->getSchemeAndHttpHost();
     $current_path = $this->currentPathStack->getPath();
     $tweet = urlencode($service_status . $node_title . ' ' . $host . $current_path);
