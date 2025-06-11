f6f8bcd8aa09d2e1b74dba3c653e254bc9708376
protitude <miles.france@gmail.com>
2025-06-11 11:43:42 -0600

    Add loading message to service_alert and news

---
+++ js/loading.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var loadingMessage = document.querySelector(".loading-message");
+  if (loadingMessage) {
+    loadingMessage.remove();
+  }
+});
