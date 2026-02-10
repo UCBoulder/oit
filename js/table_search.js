fde98ad3198df4b416fd0892a0a6c06e89ecff0a
protitude <miles.france@gmail.com>
2026-02-10 14:16:27 -0700

    table_search: #1172
    Make search work with multiple tables on the same page

--- js/table_search.js
+++ js/table_search.js
@@ -1,24 +1,30 @@
 (function ($) {
   $(document).ready(function () {
     if ( $( ".table-search" ).length ) {
-      var tableId = $('.table-search').next().attr('id');
-      $('.table-search').before('<label for="searchtable"><strong>Enter keyword to search </strong></label><input id="searchtable" type="search" autosave="csrsearch" results="1" placeholder="search" name="s">');
-      $('#searchtable').keyup(function () {
-        searchTable($(this).val(), tableId);
-      });
-    }
-    if ( $( ".page-node-1485" ).length ) {
-      $('.table-search').before('<select class="cat_search" style="margin-left: .5em;" name="cat"><option value="">Choose From Categories</option><option value="">--- Room Features ---</option><option value="RCC">Remote-Capable Classrooms (RCC)</option><option value="ME ">Media Equipped (ME)</option><option value="SC">Smart Classrooms (SC)</option><option value="ALR">Active Learning Rooms (ALR)</option><option value="LLH">Large Lecture Hall (LLH)</option><option value="DL">Distance Learning (DL)</option><option value="CC">Classroom Capture (CC)</option><option value="CR">CUClickers Receivers (CR)</option><option value="WM">Wireless Microphones (WM)</option><option value="LAB">Computing Labs (LAB)</option><option value="">--- Building Type ---</option><option value="academic">Academic</option></select>');
-      $( ".cat_search" ).change(function () {
-        var selection = this.value;
-        if (selection) {
-          $('#searchtable').val(selection);
-          searchTable($(this).val(), 'gdoc-table');
-        } else {
-          $('#searchtable').val('');
-          searchTable($(this).val(), 'gdoc-table');
+      $('.table-search').each(function(i, elem) {
+        var $ts = $(elem);
+        var tableId = $ts.next().attr('id');
+        var inputId = 'searchtable-' + i;
+        $ts.before('<label for="' + inputId + '"><strong>Enter keyword to search </strong></label><input id="' + inputId + '" type="search" autosave="csrsearch" results="1" placeholder="search" name="s">');
+        $('#' + inputId).keyup(function () {
+          searchTable($(this).val(), tableId);
+        });
+
+        if ( $( ".page-node-1485" ).length ) {
+          var selectHtml = '<select class="cat_search" style="margin-left: .5em;" name="cat"><option value="">Choose From Categories</option><option value="">--- Room Features ---</option><option value="RCC">Remote-Capable Classrooms (RCC)</option><option value="ME ">Media Equipped (ME)</option><option value="SC">Smart Classrooms (SC)</option><option value="ALR">Active Learning Rooms (ALR)</option><option value="LLH">Large Lecture Hall (LLH)</option><option value="DL">Distance Learning (DL)</option><option value="CC">Classroom Capture (CC)</option><option value="CR">CUClickers Receivers (CR)</option><option value="WM">Wireless Microphones (WM)</option><option value="LAB">Computing Labs (LAB)</option><option value="">--- Building Type ---</option><option value="academic">Academic</option></select>';
+          $ts.before(selectHtml);
+          $ts.prev('.cat_search').change(function () {
+            var selection = this.value;
+            if (selection) {
+              $('#' + inputId).val(selection);
+              searchTable(selection, 'gdoc-table');
+            } else {
+              $('#' + inputId).val('');
+              searchTable(selection, 'gdoc-table');
+            }
+          }).change();
         }
-      }).change();
+      });
     }
   });
 
